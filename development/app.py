ccc0c2de2f1e5acf1a4b9d64b44b3a5e3eec4427
Tina Wu <tinawu.nd23@gmail.com>
2019-02-16 11:55:22 -0800

    accept post request

--- development/app.py
+++ development/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +15,25 @@ def start():
 @app.route('/customer')
 def customer():
     return render_template('buyer.html')
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        print("received request")
+        zipcode = request.form['zipcode']
+        radius = request.form['radius']
+        print("{} {}".format(zipcode, radius))
+        url = "https://www.zipcodeapi.com/rest/RzOSXX9PvVlojd25uKLhCsQs5IByWMJMsFj0Lbt1pRYo1CQSSDeRW2BeLZN69idK/radius.json/{}/{}/mile".format(
+            str(zipcode), str(radius))
+        source = requests.get(url)
+        data = source.text
+        zips = json.loads(data)['zip_codes']
+
+        for zipcodes in zips:
+            print(zipcodes['zip_code'], zipcodes['distance'])
+
+        return render_template('search.html', zips)
+
 
 @app.route('/business')
 def business():
